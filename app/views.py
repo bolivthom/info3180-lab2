9aa96abb80a17f874d5769d9061c185462403bf7
Brittany <bolivthom@gmail.com>
2019-01-27 21:42:46 +0000

    fixing date

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime 
+from datetime import date
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -24,10 +25,20 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/profile')
+def profile():
+    """Render website's home page."""
+    return render_template('profile.html')
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+def format_date_joined():
+    date_joined = datetime.date(2019, 2, 3)
+    return "Joined "  + date_joined.strftime("%B, %Y") 
+
+date = format_date_joined()
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
